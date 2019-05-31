f58c033bb929386a26e515775f634d174aaaeac2
Cody Jacques <cjacques@wayfair.com>
2019-05-31 12:41:49 -0400

    scaffolding of first page class.

---
+++ base_classes/base_scraper.py
@@ -0,0 +1,39 @@
+
+import requests
+import traceback
+from bs4 import BeautifulSoup
+
+headers = {
+    'Content-type': 'text/html',
+    'Accept': 'application/json',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+}
+
+
+class BaseScraper:
+
+    def __init__(self, url_to_scrape):
+        self.url_to_scrape = url_to_scrape
+        self.dom = self.get_dom_from_url()
+
+    def get_dom_from_url(self):
+        try:
+            resp = requests.get(self.url_to_scrape, headers=headers, timeout=2)
+            soup = BeautifulSoup(resp.text, 'html')
+        except Exception as e:
+            traceback.print_exc()
+            raise e
+
+        return soup
+
+    def get_current_for_sale(self):
+        pass
+
+    def get_current_wanted_by(self):
+        pass
+
+    def get_issue_facts(self):
+        pass
+
+    def get_story_summary(self):
+        pass
---
+++ comicspriceguide/issue_page.py
@@ -0,0 +1,8 @@
+
+from base_classes.base_scraper import BaseScraper
+
+class IssuePage(BaseScraper):
+
+    def __init__(self):
+        pass
+
